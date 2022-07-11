fac7d98cb24a2d0cb127a331a2388406a3a0d721
Konstantin Pachuev <pachuev@gmail.com>
2022-07-11 13:25:07 +0300

    adds find and print

--- range/main.cpp
+++ range/main.cpp
@@ -25,13 +25,44 @@ void PrintRange(It range_begin, It range_end) {
 }
 */
 
+template <typename Container, typename Item>
+void FindAndPrint(Container container, const Item& item) {
+    auto found = find(container.begin(), container.end(), item);
+    PrintRange(container.begin(), found);
+    PrintRange(found, container.end());
+}
+
+/*
+template <typename Container, typename Element>
+void FindAndPrint(const Container& container, const Element& element_to_find) {
+    auto it_begin = begin(container);
+    auto it_end = end(container);
+    auto it = find(it_begin, it_end, element_to_find);
+    PrintRange(it_begin, it);
+    PrintRange(it, it_end);
+}
+*/
 
 int main() {
-    cout << "Test1"s << endl;
-    set<int> test1 = { 1, 1, 1, 2, 3, 4, 5, 5 };
-    PrintRange(test1.begin(), test1.end());
-    cout << "Test2"s << endl;
-    vector<int> test2 = {}; // пустой контейнер
-    PrintRange(test2.begin(), test2.end());
-    cout << "End of tests"s << endl;
+
+    { // Print
+        cout << "Test1"s << endl;
+        set<int> test1 = { 1, 1, 1, 2, 3, 4, 5, 5 };
+        PrintRange(test1.begin(), test1.end());
+        cout << "Test2"s << endl;
+        vector<int> test2 = {}; // пустой контейнер
+        PrintRange(test2.begin(), test2.end());
+        cout << "End of tests"s << endl;
+    }
+
+
+    { // Find and print
+        set<int> test = { 1, 1, 1, 2, 3, 4, 5, 5 };
+        cout << "Test1"s << endl;
+        FindAndPrint(test, 3);
+        cout << "Test2"s << endl;
+        FindAndPrint(test, 0); // элемента 0 нет в контейнере
+        cout << "End of tests"s << endl;
+    }
+
 }
